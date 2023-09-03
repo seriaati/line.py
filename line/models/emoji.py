9572b853d3b06554fafc091a2c28a3e4a31303e1
seriaati <seria.ati@gmail.com>
2023-09-03 15:30:39 +0800

    fix: emoji subclassing line model

--- line/models/emoji.py
+++ line/models/emoji.py
@@ -1,7 +1,4 @@
-import linebot.v3.messaging as messaging
-
-
-class Emoji(messaging.Emoji):
+class Emoji:
     def __init__(self, product_id: str, emoji_id: str) -> None:
         self.product_id = product_id
         self.emoji_id = emoji_id
