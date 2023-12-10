778d63b15e9d7eddc773c69e1e8bd8d8b55b7a4b
seriaati <seria.ati@gmail.com>
2023-12-10 12:58:07 +0800

    refactor: Replace `setattr` with direct value assignment

--- line/command.py
+++ line/command.py
@@ -5,7 +5,7 @@ def command(func: Callable[..., Awaitable[None]]):
     async def wrapper(*args, **kwargs):
         await func(*args, **kwargs)
 
-    setattr(wrapper, "__is_command__", True)
+    wrapper.__is_command__ = True
     wrapper.original_function = func
 
     return wrapper
