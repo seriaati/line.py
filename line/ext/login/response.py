9c2c0d8a98a1826303569894024ee08ae1e7e134
seria <seria.ati@gmail.com>
2025-06-17 11:30:35 +0900

    fix: Remove default None for UserProfile model

--- line/ext/login/response.py
+++ line/ext/login/response.py
@@ -6,5 +6,5 @@ from pydantic import BaseModel, Field
 class UserProfile(BaseModel):
     user_id: str = Field(alias="userId")
     display_name: str = Field(alias="displayName")
-    picture_url: str = Field(None, alias="pictureUrl")
-    status_message: str = Field(None, alias="statusMessage")
+    picture_url: str = Field(alias="pictureUrl")
+    status_message: str = Field(alias="statusMessage")
