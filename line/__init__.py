09829316adc4ddb88dcac891703483a82850555e
seria <seria.ati@gmail.com>
2026-03-09 16:33:35 +0800

    fix: Fix missing Cog export

--- line/__init__.py
+++ line/__init__.py
@@ -1,5 +1,6 @@
 from . import models
 from .bot import Bot as Bot
+from .bot import Cog as Cog
 from .command import command as command
 from .context import Context as Context
 from .exceptions import *
